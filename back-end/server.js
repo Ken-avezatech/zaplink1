bc66d1d747e47cfa020d928860f6770b911c14a7
Jo-Ries Canino <canino_jories@hotmail.com>
2016-02-21 15:06:17 +0800

    changing pingtimeout

--- back-end/server.js
+++ back-end/server.js
@@ -33,7 +33,7 @@
   var sticky = require('sticky-session');
 
   let server = io.http.createServer(app);
-  global.socket_io = require('socket.io')(server, {pingTimeout: 3000, pingInterval: 3000});
+  global.socket_io = require('socket.io')(server, {pingTimeout: 3000, pingInterval: 5000});
   server.listen(io.port, function() {
     /*TODO*/
     /**
